953fdb59f31f7151ced50f4dd51db7de5771b928
amir <amirfayazfanavar@gmail.com>
2022-07-31 06:50:26 -0400

    feat: add community controller and model

---
+++ app/Http/Controllers/CommunityController.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Community;
+use Illuminate\Console\Command;
+use Illuminate\Http\Request;
+use App\Models\Topic;
+use Illuminate\Support\Facades\Cache;
+use App\Http\Requests\Community\StoreCommunityRequest;
+
+class CommunityController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        $topics = Cache::get('topics',function(){  Topic::all();    });
+        return view('communities.create',compact('topics'));
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreCommunityRequest $request)
+    {
+        $community = Community::create($request->validated() + ['user_id' => auth()->user()->id]);
+        return redirect()->route('communities.show',[$community->id]);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        return "testing";
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
