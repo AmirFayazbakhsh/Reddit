e0667d46ee156d534b3c8addc5b9a014c3e1a783
amir <amirfayazfanavar@gmail.com>
2022-08-01 02:43:21 -0400

    feat: add routes and middleware

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\CommunityController;
+use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -21,8 +23,8 @@ Auth::routes(['verify' => true]);
 
 Route::middleware(['auth','verified'])->group(function(){
 
-    Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+    Route::get('/home', [HomeController::class, 'index'])->name('home');
+    Route::resource('communities', CommunityController::class);
 
-}
-);
+});
 
